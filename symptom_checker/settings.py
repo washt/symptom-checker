c98d9bf1a34b0ea4449aa6464baaff3ee9b5cd73
Tucker Wash <tucker@songbird.sh>
2022-05-10 21:06:20 -0400

    add and configure corsheaders package

--- symptom_checker/settings.py
+++ symptom_checker/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'djangorestframework',
+    'corsheaders',
     'rare_conditions'
 ]
 
@@ -49,6 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
 ]
 
 ROOT_URLCONF = 'symptom_checker.urls'
